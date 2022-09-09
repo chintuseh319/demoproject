6c6ab6beaf87adde0286a1b079fcacd718bdb1cc
Sai <chintuseh319@gmail.com>
2022-09-09 19:43:33 +0530

    program change

---
+++ demonew.py/demo.py
@@ -0,0 +1,11 @@
+import time
+for x in range(15):
+        if (x>=30):
+                print("Dear user please stop here.....")
+                break
+else:
+
+        print("The values are successfully done")
+print()        
+time.sleep(1)
+print("End of an application.........")
